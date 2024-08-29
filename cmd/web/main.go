59182ef23167b67eeac61df3e1a2fc28d4f85807
Soyaib10 <soyaibzihad10@gmail.com>
2024-08-29 14:24:04 +0600

    3- leveled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,7 +12,9 @@ func main() {
 	addr := flag.String("addr", ":8080", "HTTP network address")
 	flag.Parse()
 
-
+	// creating loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -21,7 +24,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Staritng server on %s", *addr)
+	infoLog.Printf("Staritng server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
