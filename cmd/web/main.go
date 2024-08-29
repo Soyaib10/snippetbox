29a35bab998f2172416596c0ee669d83532b38f6
Soyaib10 <soyaibzihad10@gmail.com>
2024-08-29 14:16:05 +0600

    3- adding flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	// Define a new command-line flag with the name 'addr'
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	flag.Parse()
 
+
+
+	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
@@ -15,7 +21,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :8080")
-	err := http.ListenAndServe(":8080", mux)
+	log.Printf("Staritng server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
