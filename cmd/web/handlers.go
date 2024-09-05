3219c82e452669230eff8bd4d5a58c4aae82873d
Soyaib10 <soyaibzihad10@gmail.com>
2024-09-05 15:19:53 +0600

    6- removing the deliberate panic

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,8 +15,6 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	panic("oops! something went so wrong") // Deliberate panic
-
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
