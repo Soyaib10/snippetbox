f7e260d1d7ce9723485e814b60f0e7f3375e8fe1
Soyaib10 <soyaibzihad10@gmail.com>
2024-09-05 15:19:26 +0600

    6- recovering panic through middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // secureHeaders → servemux → application handler
 func secureHeaders(next http.Handler) http.Handler {
@@ -19,5 +22,19 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 
         next.ServeHTTP(w, r)
     })
+}
+
+// recoverPanic recovers panic
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred function (which will always be run in the event of a panic as Go unwinds the stack).
+		defer func ()  {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
 }
 
