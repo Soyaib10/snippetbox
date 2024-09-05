fb70f7d6f1f92a683f4d4e4ce72bf83f1ed2d4d8
Soyaib10 <soyaibzihad10@gmail.com>
2024-09-05 15:33:02 +0600

    6- added justinas alice middleware package

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,15 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // routes now retrurn http.Handler instead of *http.ServeMux.
 func (app *application) routes() http.Handler {
+	// Create a middleware chain containing our 'standard' middleware which will be used for every request our application receives.
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
-	
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/Soyaib10/snippetbox
 
 go 1.18
 
-require github.com/go-sql-driver/mysql v1.8.1
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/justinas/alice v1.2.0
+)
 
 require filippo.io/edwards25519 v1.1.0 // indirect
