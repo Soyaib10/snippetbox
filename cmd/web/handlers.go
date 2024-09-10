20b56585c3398567b831e342b0001f81955ef405
Soyaib10 <soyaibzihad10@gmail.com>
2024-09-10 22:40:28 +0600

    11- cleaning

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -58,25 +58,6 @@ func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
-	// err := r.ParseForm()
-	// if err != nil {
-	// 	app.clientError(w, http.StatusBadRequest)
-	// 	return
-	// }
-
-	// form := forms.New(r.PostForm)
-	// id, err := app.users.Authenticate(form.Get("email"), form.Get("password"))
-	// if err == models.ErrInvalidCredentials {
-	// 	form.Errors.Add("generic", "Email or Password is incorrect")
-	// 	app.render(w, r, "login.page.tmpl", &templateData{Form: form})
-	// 	return
-	// } else if err != nil {
-	// 	app.serverError(w, err)
-	// 	return
-	// }
-
-	// app.session.Put(r, "userID", id)
-	// http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 	err := r.ParseForm()
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
